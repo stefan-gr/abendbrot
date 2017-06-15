8ca7aac9be0d8f3b332474b0cd29d4c77265bae5
stefan-gr <stefan-gr@users.noreply.github.com>
2017-06-15 13:46:01 +0200

    tools/update_libretro.sh: Add Conditional requests to not exceed rate limit, #68

--- tools/update_libretro.sh
+++ tools/update_libretro.sh
@@ -2,10 +2,10 @@
 # This script updates all libretro cores and misc retroarch packages
 # to the newest date. It downloads https://api.github.com/repos.
 # Please note: this API has a rate limit of at least 60 requests per hour.
-# It should be enough for this.
+# It should be enough for this provided not every single core needs to be updated.
 
-# TODO: Download libretro-super and place info files into ${FILESDIR}
-# TODO: Clean this up.
+#TODO: Fix ebuilds not cantaining *libretro*, they update without proper commit check
+#TODO: See line #46 for culprit
 
 # Make sure we have the needed commands
 command -v curl >/dev/null 2>&1 || { echo >&2 "Please install net-misc/curl. Aborting."; exit 1; }
@@ -30,17 +30,35 @@ then
 fi
 
 core_update() {
-	local CORE_DIR="${@%/*}"
-	local CORE_NAME="${@##*/}"
-	# Assign repo uri for later use
-	local `grep HOMEPAGE= "${@}"`
+	CORE_DIR="${@%/*}"
+	CORE_NAME="${@##*/}"
+	export LC_ALL=C
+	export TZ=GMT
+
 	# Do some crude string expansion to get github abi uri.
+	local `grep HOMEPAGE= "${@}"`
 	local EGIT_URI="${HOMEPAGE:1:${#HOMEPAGE}-2}"
 	local EGIT_URI="${EGIT_URI/git:\/\//https:\/\/}"
 	local EGIT_COMMITS_URI="${EGIT_URI/https:\/\/github.com/https:\/\/api.github.com\/repos}"
 	local EGIT_COMMITS_URI="${EGIT_COMMITS_URI}/commits"
-	# Do some crude string expansion to get latest commit date and sha sum through github abi.
-	local LATEST_COMMIT_CURL=$(curl -i "${EGIT_COMMITS_URI}")
+
+	# Check for latest ebuild date and use it as a base to get the latest commit.
+	local EGIT_CURRENT_DATE=$(for n in "${CORE_DIR}"/*libretro*
+	do
+		[[ $n == *"9999"* ]] && continue
+		echo "$n"
+	done | sort | tail -1)
+	echo "${EGIT_CURRENT_DATE}"
+	exit 1
+	if [[ -z "${EGIT_CURRENT_DATE}" ]]
+	then
+		local LATEST_COMMIT_CURL=$(curl -i "${EGIT_COMMITS_URI}")
+	else
+		local EGIT_CURRENT_DATE=$(sed 's/^.*\([0-9]\{8\}\).*$/\1/' <<< "${EGIT_CURRENT_DATE}")
+		# Do some crude string expansion to get latest commit date and sha sum through github abi.
+		local LATEST_COMMIT_CURL=$(curl -i "${EGIT_COMMITS_URI}" -H "If-Modified-Since: $(date --date="${EGIT_CURRENT_DATE} 23:59:59" +"%a, %d %b %Y %H:%M:%S %Z")")
+		[[ "${LATEST_COMMIT_CURL}" == *"304 Not Modified"* ]] && echo -e "\e[33m\"${CORE_NAME%%-9*}-1.0_pre${EGIT_CURRENT_DATE}\" already up-to-date.\e[0m" && return
+	fi
 	local LATEST_COMMIT_DATE=$(echo "${LATEST_COMMIT_CURL}" | grep date | head -1)
 	local LATEST_COMMIT_SHA=$(echo "${LATEST_COMMIT_CURL}" | grep sha | head -1)
 	# Strip the variables of junk
@@ -54,7 +72,7 @@ core_update() {
 		echo -e "\e[31mFetching repo information failed. Rate limit probably exceeded, wait 1 hour\e[0m"
 		echo -e "\e[31mTry 'curl -i \"${EGIT_COMMITS_URI}\"'\e[0m"
 		echo -e "\e[31mTo see what problem it is.\e[0m"
-		exit 1
+		return
 	fi
 	# Prepare ebuild name and path
 	local LATEST_COMMIT_DATE_VERSION="${LATEST_COMMIT_DATE:0:4}${LATEST_COMMIT_DATE:5:2}${LATEST_COMMIT_DATE:8:2}"
@@ -75,16 +93,22 @@ core_update() {
 	fi
 }
 
-# Collect all needed files and send them to core_update
-for core in "${SCRIPT_DIR}/../games-emulation/"*libretro*/*-9999*
-do
-	# Skip libretro-meta package
-	[[ $core == *"libretro-meta"* ]] && continue
-	core_update "${core}"
-done
-# Didn't catch them above
-core_update "${SCRIPT_DIR}/../games-emulation/retroarch-assets/retroarch-assets-9999-r2.ebuild"
-core_update "${SCRIPT_DIR}/../games-emulation/retroarch-joypad-autoconfig/retroarch-joypad-autoconfig-9999-r2.ebuild"
-core_update "${SCRIPT_DIR}/../games-emulation/common-overlays/common-overlays-9999-r2.ebuild"
-core_update "${SCRIPT_DIR}/../games-emulation/common-shaders/common-shaders-9999-r2.ebuild"
-#core_update "${@}"
+if [ -z "$1" ]
+then
+	# Collect all needed files and send them to core_update
+	for core in "${SCRIPT_DIR}/../games-emulation/"*libretro*/*-9999*
+	do
+		# Skip libretro-meta package
+		[[ $core == *"libretro-meta"* ]] && continue
+		core_update "${core}"
+	done
+	# Didn't catch them above
+	core_update "${SCRIPT_DIR}/../games-emulation/retroarch-assets/retroarch-assets-9999-r2.ebuild"
+	core_update "${SCRIPT_DIR}/../games-emulation/retroarch-joypad-autoconfig/retroarch-joypad-autoconfig-9999-r2.ebuild"
+	core_update "${SCRIPT_DIR}/../games-emulation/common-overlays/common-overlays-9999-r2.ebuild"
+	core_update "${SCRIPT_DIR}/../games-emulation/common-shaders/common-shaders-9999-r2.ebuild"
+	core_update "${SCRIPT_DIR}/../games-emulation/glsl-shaders/glsl-shaders-9999-r2.ebuild"
+	core_update "${SCRIPT_DIR}/../games-emulation/slang-shaders/slang-shaders-9999-r2.ebuild"
+else
+	core_update "${@}"
+fi
